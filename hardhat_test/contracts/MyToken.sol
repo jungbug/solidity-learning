f58effd0605bebeb0c53b3cb1b1b2d1810732104
jungbug <xbox1111@naver.com>
2026-04-12 23:26:17 +0900

    added transfer

--- hardhat_test/contracts/MyToken.sol
+++ hardhat_test/contracts/MyToken.sol
@@ -9,16 +9,22 @@ contract MyToken{
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
 
-    constructor(string memory _name, string memory _symbol, uint8 _decimal) {
+    constructor(string memory _name, string memory _symbol, uint8 _decimal, uint256 _amount) {
         name = _name;
         symbol = _symbol;
         decimals = _decimal;
-        _mint(1000000000000000000, msg.sender); // 1 MT 발생함 sender에게 보낸다.
+        _mint(_amount * 10 ** uint256(decimals), msg.sender);
     }
 
     function _mint(uint amount, address owner) internal {
         totalSupply += amount;
         balanceOf[owner] += amount;
+    }
+
+    function transfer(uint256 amount, address to) external {
+        require(balanceOf[msg.sender] >= amount, "insufficient balance"); // uint 256은 음수가 없는데 음수를 만들면서 에러가 뜨기 때문에 방지
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
     }
 
     // function totalSupply() external view returns (uint256) {
